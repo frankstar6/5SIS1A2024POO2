6a23c3ad8255391d25a87ebdd84d67003d01d350
frankstar6 <franciscoestrella631@gmail.com>
2024-01-18 21:29:48 -0600

    HolaMundo

---
+++ HolaMundo.java
@@ -0,0 +1,23 @@
+
+
+// vamos a crear un primer programa
+
+/* para poder comentar en diferentes lineas de codigo */
+
+public class HolaMundo {
+
+    // Los metodos son los comportamientos que va a tener una clase u objeto. El comportamiento para las que esta destinada la clase
+
+    // existen metodos principales (main)
+    // metodos getter and setter intercambio de datos 
+    // metodos constructores Los constructores son los encargados de la estructura de la clase, inicializar las variables, crear las instancias 
+    //metodos por tipo de dato nos referimos a it, float, string, etc
+
+    //Los objetos son instancias (invocaciones) La construccion abstracta de la cosa 
+
+    //Las caracteristicas del objeto son los atributos de la clase
+
+    public static void main (String[] args){
+System.out.println("Si sale error");
+    }
+}
